e6dd83209e8eee57e152388c102cca35b8f9fe1a
Shyam Kumar <sykron@gmail.com>
2019-02-13 00:09:15 +0800

    Basic Thymeleaf

--- src/main/java/com/vega/springit/controller/HomeController.java
+++ src/main/java/com/vega/springit/controller/HomeController.java
@@ -9,10 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HomeController {
 
-    @GetMapping(path = "/")
-    public String home(Model model, HttpServletRequest request) {
-        model.addAttribute("message", "Hello World!!!");
-        return "index";
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Hello, ThymeLeaf");
+        return "home";
     }
 
     //@PostMapping etc...
