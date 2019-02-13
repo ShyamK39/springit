597b0417fe4b8b352e4e3ff910ed475eef0bd769
Shyam Kumar <sykron@gmail.com>
2019-02-14 01:20:11 +0800

    Actuator Security Added

--- src/main/java/com/vega/springit/security/SecurityConfiguration.java
+++ src/main/java/com/vega/springit/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.vega.springit.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,9 +25,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                    .antMatchers("/").permitAll()
-                    .antMatchers("/link/submit").hasRole("USER")
+                .authorizeRequests().requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                .antMatchers("/actuator/").hasRole("ACTUATOR")
+                .antMatchers("/").permitAll()
+                .antMatchers("/link/submit").hasRole("USER")
                 .and()
                 .formLogin();
     }
