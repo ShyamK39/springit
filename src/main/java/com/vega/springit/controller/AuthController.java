bb7157162a7ff3879cd73b8df691a8bd20568ca9
Shyam Kumar <sykron@gmail.com>
2019-02-15 05:13:09 +0800

    Thymeleaf Spring Security Dialect + CreatedBy field and Register and Profile pages

--- src/main/java/com/vega/springit/controller/AuthController.java
+++ src/main/java/com/vega/springit/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.vega.springit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class AuthController {
     @RequestMapping(value = {"/login", "/auth/login", "auth/login.html"})
     public String login() {
         return "auth/login";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "auth/profile";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
     }
 }
